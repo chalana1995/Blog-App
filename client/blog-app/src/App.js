0328697235392b643123c9e8b808cd9e8e02c556
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-10-12 09:17:32 +0530

    routing set

--- client/blog-app/src/App.js
+++ client/blog-app/src/App.js
@@ -1,11 +1,34 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/post/:id", element: <Single /> },
+      { path: "/write", element: <Write /> },
+    ],
   },
   {
     path: "/register",
