7e4cd28810128e199f251d746a713e4a0aae3e0c
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-10-15 23:45:47 +0530

    create controllers and routers

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/post", postRoutes);
 
 app.listen(8800, () => {
   console.log("Server running");
