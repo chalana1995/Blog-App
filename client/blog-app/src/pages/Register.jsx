2701a1c5bdd9a55d9d3a972b45ad8af38c744fad
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-10-17 12:20:25 +0530

    fwfefwf

--- client/blog-app/src/pages/Register.jsx
+++ client/blog-app/src/pages/Register.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [input, setInput] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputs = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.name]: e.taget.value }));
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
+        <input
+          required
+          type="text"
+          name="username"
+          value={input.username}
+          onChange={handleInputs}
+          placeholder="username"
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          value={input.email}
+          onChange={handleInputs}
+          placeholder="email"
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          value={input.password}
+          onChange={handleInputs}
+          placeholder="password"
+        />
         <button>Register</button>
         <p>This is an Error</p>
         <span>
